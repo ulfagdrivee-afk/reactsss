42d5ecce85fed841a04b388543e6a52ce7acf32b
Jefri225 <jefrislay@gmail.com>
2026-05-02 15:02:24 +0800

    mk

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import axios from "axios";
+class Home extends Component {
+    render() {
+        return (
+            <div className="container mt-5">
+                <h1>Selamat datang di aplikasi keuangan</h1>
+
+                
+            </div>
+        )
+    }
+}
